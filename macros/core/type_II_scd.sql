a786b80a2b4ae961981536fbbb6d867c734897e9
jnritter <59490758+jnritter@users.noreply.github.com>
2026-02-12 10:57:04 -0500

    Update type_II_scd.sql

--- macros/core/type_II_scd.sql
+++ macros/core/type_II_scd.sql
@@ -1,49 +1,48 @@
-{%- macro fix_scd_end_dates(source_table, partition_by) -%}
+{%- macro fix_scd_end_dates(source_table, partition_by, row_contents_hash='hash_key', start_date_col='start_date', end_date_col='end_date') -%}
 
 {# This macro is for fixing the start and end dates of a dbt snapshot that has been already been standardized into a date-delineated Type II SCD table. 
   Call it as a post-hook and let it work. 
-  The partition_by parameter should be at the grain of the entities whose history is being tracked.
-  There must be a column called start_date, and a column called end_date.
+  The entity_key parameter should be at the grain of the entities whose history is being tracked.
   #}
 {%- set key_cols = partition_by | join(', ') -%}
 
 {% set merge_statement %}
 
-merge into {{ source_table }} dst using(
-with new_start_date as (
+merge into {{ source_table }} dst using (
+with new_{{ start_date_col }} as (
     select
         {{ key_cols }},
-        start_date,
-        end_date,
+        {{ start_date_col }},
+        {{ end_date_col }},
         case
-            when hash_key = lag(hash_key) over(partition by {{ key_cols }} order by start_date)
+            when {{ row_contents_hash }} = lag({{ row_contents_hash }}) over(partition by {{ key_cols }} order by {{ start_date_col }})
                 then null
-            else start_date
-        end as start_date_new
+            else {{ start_date_col }}
+        end as {{ start_date_col }}_new
     from {{ source_table }}
 ),
 
-new_end_date as (
+new_{{ end_date_col }} as (
     select
         *,
         coalesce(dateadd(day, -1,
-                        lag(start_date_new) over(partition by {{ key_cols }} order by start_date_new desc)),
+                        lag({{ start_date_col }}_new) over(partition by {{ key_cols }} order by {{ start_date_col }}_new desc)),
                 to_date('9999-12-31')
-        ) as end_date_new
-    from new_start_date 
+        ) as {{ end_date_col }}_new
+    from new_{{ start_date_col }} 
 )
 
-select * from new_end_date
+select * from new_{{ end_date_col }}
 
 ) src
     on {% for col in partition_by %}
     dst.{{ col }} = src.{{ col }} and 
     {%- endfor %} 
-    dst.start_date = src.start_date
-when matched and src.start_date_new is null then 
+    dst.{{ start_date_col }} = src.{{ start_date_col }}
+when matched and src.{{ start_date_col }}_new is null then 
     delete
 when matched then update set 
-    dst.end_date = src.end_date_new
+    dst.{{ end_date_col }} = src.{{ end_date_col }}_new
 
 {% endset %}
 
@@ -56,8 +55,6 @@ when matched then update set
   {% set results = [] %}
 
 {% endif %}
-
-{%- endmacro -%}
 
 
 {%- macro snapshot_to_scd2(entity_key, snapshot_ref, valid_from_date_col_names=['dbt_valid_from','start_date'], valid_to_date_col_names=['dbt_valid_to','end_date'], end_date_default='9999-12-31') -%}
